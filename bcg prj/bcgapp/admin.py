30b88e5d1acca8f73cfe8aaf0999763beab0730d
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-02-13 09:38:00 +0530

    Whole project

--- bcg prj/bcgapp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import empdata
-# Register your models here.
-admin.site.register(empdata)
--- bcg prj/bcgapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BcgappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bcgapp'
